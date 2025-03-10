768df35898355129f3a4572b2e65354c7ebb3e26
Ameer Tufail <118817158+ameer22l7555@users.noreply.github.com>
2025-03-11 01:11:03 +0500

    Add second source file Source.cpp

---
+++ src/Source.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//#include <chrono>
+//#include <fstream>
+//#include <cstring>
+//#include <Windows.h>
+//using namespace std;
+//int Play();
+//int input(int);
+//void converter();
+//void pawn(int);
+//void bishop(int);
+//void rook(int);
+//void queen(int);
+//void horse(int);
+//void king(int);
+//int check();
+//void boardload();
+//void loading();
+//int game();
+//int menu();
+//ofstream fin;
+//ifstream fout;
+//int srow, scol;
+//int erow, ecol;
+//char start[10];
+//char board[8][8] = {
+//	'R', 'H', 'B', 'Q', 'K', 'B', 'H', 'b',
+//	'P', 'P', 'P', 'P', 'P', 'P', '0', 'P',
+//	' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+//	' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+//	' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+//	' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+//	'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',
+//	'r', 'h', 'b', 'q', 'k', 'b', 'h', 'r'
+//};
